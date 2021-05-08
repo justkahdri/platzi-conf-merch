18a2a9f705ff091a4a73efcc97dff727c997f33c
Joaquin Montes <joaco.rm512@gmail.com>
2021-05-08 14:47:36 -0300

    App Arch designed

---
+++ src/routes/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import {Home, Checkout, Information, Payment, Success, NotFound} from '../containers';
+
+const App = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/checkout' component={Checkout} />
+            <Route exact path='/checkout/info' component={Information} />
+            <Route exact path='/checkout/payment' component={Payment} />
+            <Route exact path='/checkout/success' component={Success} />
+            <Route component={NotFound} />
+        </Switch>
+    </BrowserRouter>
+);
+
+export default App;
