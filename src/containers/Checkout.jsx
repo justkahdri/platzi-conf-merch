aea12556a814fefea8209e7d8002a5259463fb7f
Joaquin Montes <joaco.rm512@gmail.com>
2021-05-10 10:40:32 -0300

    Added Checkout

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
+import '../styles/components/Checkout.css';
+
 const Checkout = () => (
-    <h1>Checkout</h1>
+    <div className="checkout">
+        <section className="checkout-content">
+            <h3>Lista de Pedidos:</h3>
+            <div className="checkout-item">
+                <div className="checkout-element">
+                    <h4>ITEM NAME</h4>
+                    <span>$10</span>
+                </div>
+                <button type='button'>Eliminar</button>
+            </div>
+        </section>
+        <section className="checkout-sidebar">
+            <h3>Precio Total: $10</h3>
+            <button type="button">Continuar pedido</button>
+        </section>
+    </div>
 );
 
 export default Checkout;
