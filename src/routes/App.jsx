6d300f0dbcb1874e4cd7bbd86e006f99c9df36e4
Joaquin Montes <joaco.rm512@gmail.com>
2021-05-11 12:21:11 -0300

    - Added first custom hook - Added AppProvider

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,22 +3,25 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Layout from '../components/Layout';
 import {Home, Checkout, Information, Payment, Success, NotFound} from '../containers';
+import AppProvider from '../context/AppContext';
 
 import '../styles/components/App.css';
 
 const App = () => (
-    <BrowserRouter>
-    <Layout>
-        <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/checkout' component={Checkout} />
-            <Route exact path='/checkout/info' component={Information} />
-            <Route exact path='/checkout/payment' component={Payment} />
-            <Route exact path='/checkout/success' component={Success} />
-            <Route component={NotFound} />
-        </Switch>
-        </Layout>
-    </BrowserRouter>
+    <AppProvider>
+        <BrowserRouter>
+            <Layout>
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/checkout' component={Checkout} />
+                    <Route exact path='/checkout/info' component={Information} />
+                    <Route exact path='/checkout/payment' component={Payment} />
+                    <Route exact path='/checkout/success' component={Success} />
+                    <Route component={NotFound} />
+                </Switch>
+            </Layout>
+        </BrowserRouter>
+    </AppProvider>
 );
 
 export default App;
