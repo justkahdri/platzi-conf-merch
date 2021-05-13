9d3e41b5514cb6762ea7508dff067753b31ca94b
Joaquin Montes <joaco.rm512@gmail.com>
2021-05-13 13:18:58 -0300

    - Implementing total price with UseContext

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,30 +1,42 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
 
 import {MdDelete} from 'react-icons/md';
+import {AppContext} from '../context/AppContext';
 import '../styles/components/Checkout.css';
 
-const Checkout = () => (
+const Checkout = () => {
+    const {state: {cart}, removeFromCart} = useContext(AppContext);
+
+    const finalPrice = () => cart.reduce((sum, current) => (sum + current.price), 0);
+
+    return (
     <div className="checkout">
         <section className="checkout-content">
-            <h3>Lista de Pedidos:</h3>
-            <div className="checkout-item">
+            {cart.length ? <h3>Lista de Pedidos:</h3> : <h3>Sin pedidos...</h3>}
+
+            {cart.map((product, idx) => (
+                // eslint-disable-next-line react/no-array-index-key
+                <div className="checkout-item" key={idx}>
                 <div className="checkout-element">
-                    <h4>ITEM NAME</h4>
-                    <span>$10</span>
+                    <h4>{product.title}</h4>
+                    <span>${product.price}</span>
                 </div>
-                <button type='button'>
+                <button type='button' onClick={() => removeFromCart(product)}>
                     <MdDelete />
                 </button>
             </div>
+            ))}            
         </section>
-        <section className="checkout-sidebar">
-            <h3>Precio Total: $10</h3>
-            <Link to="/checkout/info">
-                <button type="button">Continuar pedido</button>
-            </Link>
-        </section>
+        {cart.length && (
+            <section className="checkout-sidebar">
+                <h3>{`Precio Total: $${finalPrice()}`}</h3>
+                <Link to="/checkout/info">
+                    <button type="button">Continuar pedido</button>
+                </Link>
+            </section>
+        )}
     </div>
-);
+);}
 
 export default Checkout;
